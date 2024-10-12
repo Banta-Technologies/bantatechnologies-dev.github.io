431b1c554507e1b2abf54427d018fb644fcf42a2
vicbanta <vic@bantatechnologies.com>
2024-10-12 11:33:33 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321/",
+  site: "https://banta-technologies.github.io/bantatechnologies-dev.github.io/",
   // todo - add site for sitemap
   build: {
     format: "directory",
